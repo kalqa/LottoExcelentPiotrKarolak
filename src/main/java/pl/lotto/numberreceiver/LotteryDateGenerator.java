546a79c088dee7b6ed4a28eeb4ce9d9d8588bc0e
Piotr Karolak <s18746@pjwstk.edu.pl>
2022-11-17 00:55:50 +0100

    added tests for for generatedIDLottery and GeneratedDate

--- src/main/java/pl/lotto/numberreceiver/LotteryDateGenerator.java
+++ src/main/java/pl/lotto/numberreceiver/LotteryDateGenerator.java
@@ -5,12 +5,20 @@ import java.time.LocalDateTime;
 
 class LotteryDateGenerator {
 
+    private static final String CORRECT_MESSAGE = "success";
 
     public LocalDateTime GenerateLotteryDate(String message) throws ValidationExeption {
-        if (message.equals("success"))
+        if (isCorrectMessage(message))
             return LocalDateTime.now();
-
 
         throw new ValidationExeption();
     }
+
+    private boolean isCorrectMessage(String message){
+        if (message.equals(CORRECT_MESSAGE)){
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/main/java/pl/lotto/numberreceiver/UserLotteryIdGenerator.java
+++ src/main/java/pl/lotto/numberreceiver/UserLotteryIdGenerator.java
@@ -5,13 +5,23 @@ import java.util.concurrent.atomic.AtomicLong;
 class UserLotteryIdGenerator {
 
     private static AtomicLong userLotteryId = new AtomicLong(0);
+    private static final String CORRECT_MESSAGE = "success";
 
     public Long generateUserLotteryId(String message) throws ValidationExeption {
-        if (message.equals("success")) {
+        if (isCorrect(message)) {
             userLotteryId.incrementAndGet();
             return Long.valueOf(userLotteryId.get());
         }
         throw new ValidationExeption();
+    }
+
+
+    private boolean isCorrect(String message) {
+        if (message.equals(CORRECT_MESSAGE)) {
+            return true;
+        }
+
+        return false;
     }
 
 
