90d73699e28ba571ea01bc102ea3d9096187bf3f
piotr-97 <karolak.piotr.012@gmail.com>
2022-12-30 16:00:55 +0100

    changed for each into stream check winning tickets

--- src/test/java/pl/lotto/resultchecker/ResultCheckerFacadeTest.java
+++ src/test/java/pl/lotto/resultchecker/ResultCheckerFacadeTest.java
@@ -23,7 +23,7 @@ public class ResultCheckerFacadeTest {
 
 
     @Test
-    public void should_return_winning_tickets_from_list() {
+    public void should_return_winning_tickets_list_from_all_tickets_for_draw_date() {
         // given
         NumberReceiverFacade numberReceiverFacade = Mockito.mock(NumberReceiverFacade.class);
         NumbersGeneratorFacade numbersGeneratorFacade = Mockito.mock(NumbersGeneratorFacade.class);
@@ -49,4 +49,32 @@ public class ResultCheckerFacadeTest {
         assertTrue(!winningTickets.isEmpty());
         assertTrue(winningTickets.size() == 2);
     }
+
+    @Test
+    public void should_return_empty_winning_tickets_list_from_all_tickets_for_not_winning_numbers() {
+        // given
+        NumberReceiverFacade numberReceiverFacade = Mockito.mock(NumberReceiverFacade.class);
+        NumbersGeneratorFacade numbersGeneratorFacade = Mockito.mock(NumbersGeneratorFacade.class);
+        WinnerChecker winnerChecker = new WinnerChecker();
+        LocalDateTime drawTime =  LocalDateTime.of(2022, 12, 24, 20, 0, 0);
+        ResultCheckerFacade resultCheckerFacade = new ResultCheckerFacade(numberReceiverFacade, numbersGeneratorFacade,winnerChecker);
+        List<LotteryTicketDto> winningTickets;
+        when(numbersGeneratorFacade.generateWinningNumbers()).thenReturn(new WinningNumbersDto(List.of(1, 2, 3, 4, 5, 7)));
+        when(numberReceiverFacade.usersNumbers(any())).thenReturn(
+                new AllNumbersFromUsersDto(List.of(
+                        new LotteryTicketDto(List.of(1, 2, 3, 4, 5, 6), "id", LocalDateTime.of(2022, 12, 24, 20, 0, 0)),
+                        new LotteryTicketDto(List.of(1, 22, 13, 44, 55, 76), "id2", LocalDateTime.of(2022, 12, 24, 20, 0, 0)),
+                        new LotteryTicketDto(List.of(11, 22, 33, 45, 54, 56), "id3", LocalDateTime.of(2022, 12, 24, 20, 0, 0)),
+                        new LotteryTicketDto(List.of(12, 24, 37, 44, 5, 6), "id4", LocalDateTime.of(2022, 12, 24, 20, 0, 0)),
+                        new LotteryTicketDto(List.of(11, 22, 36, 74, 53, 65), "id5", LocalDateTime.of(2022, 12, 24, 20, 0, 0)),
+                        new LotteryTicketDto(List.of(1, 2, 3, 4, 5, 6), "id6", LocalDateTime.of(2022, 12, 24, 20, 0, 0))
+                )
+                ));
+        // when
+        winningTickets = resultCheckerFacade.checkWinners(drawTime);
+
+        //then
+        assertTrue(winningTickets.isEmpty());
+    }
+
 }
