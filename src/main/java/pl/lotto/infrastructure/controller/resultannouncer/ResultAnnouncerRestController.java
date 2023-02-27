86eb6d080efd9b0405e16d37206a061d3474b6e4
Piotr Karolak <s18746@pjwstk.edu.pl>
2023-02-28 00:30:44 +0100

    resultchecker

---
+++ src/main/java/pl/lotto/infrastructure/controller/resultannouncer/ResultAnnouncerRestController.java
@@ -0,0 +1,18 @@
+package pl.lotto.infrastructure.controller.resultannouncer;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.lotto.resultannoucer.ResultAnnouncerFacade;
+
+@RestController
+public class ResultAnnouncerRestController {
+
+
+/*    private final ResultAnnouncerFacade resultAnnouncerFacade;
+
+    public  ResultAnnouncerRestController(ResultAnnouncerFacade resultAnnouncerFacade){
+        this.resultAnnouncerFacade = resultAnnouncerFacade;
+    }*/
+
+}
